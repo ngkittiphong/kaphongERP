608296bfd44fd18744211f0e668b116c5af728a6
ngkittiphong <38451192+ngkittiphong@users.noreply.github.com>
2025-02-17 22:50:58 +0700

    fix load profile

--- resources/views/livewire/user-list.blade.php
+++ resources/views/livewire/user-list.blade.php
@@ -8,20 +8,17 @@
                 <b><i class="icon-plus3"></i></b> Add new user
             </button>
         </div>
-    </div>
-
-        <!-- Search Input Field -->
-    <div class="row">
+        <div class="row">
         <input type="text" class="form-control input-lg"
             wire:model.debounce.100ms="search"
             wire:keydown.debounce.100ms="searchUsers"
             placeholder="Search user...">
-        <div class="form-control-feedback">
-            <i class="icon-search4"></i>
+                <div class="form-control-feedback">
+                    <i class="icon-search4"></i>
+                </div>
+            </div>
         </div>
     </div>
-
-
     <ul class="media-list media-list-linked p-b-5">
         @foreach ($users as $user)
         {{-- <li class="media"> --}}
